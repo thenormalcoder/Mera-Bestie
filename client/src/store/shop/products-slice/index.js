6c8bf73cd409683ab9dd20697a7a97756dea4dea
thenormalcoder <166571598+thenormalcoder@users.noreply.github.com>
2024-12-15 14:59:57 +0530

    Update index.js

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -18,7 +18,7 @@ export const fetchAllFilteredProducts = createAsyncThunk(
     });
 
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get?${query}`
+      `https://mera-bestie-six.vercel.app/products/get?${query}`
     );
 
     console.log(result);
@@ -31,7 +31,7 @@ export const fetchProductDetails = createAsyncThunk(
   "/products/fetchProductDetails",
   async (id) => {
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get/${id}`
+      `https://mera-bestie-six.vercel.app/shop/products/get/${id}`
     );
 
     return result?.data;
