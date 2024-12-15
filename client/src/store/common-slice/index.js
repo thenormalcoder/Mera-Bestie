e7e83c5deeb130541ee22043ce1f4a12b3b0296a
thenormalcoder <166571598+thenormalcoder@users.noreply.github.com>
2024-12-15 15:17:37 +0530

    Update index.js

--- client/src/store/common-slice/index.js
+++ client/src/store/common-slice/index.js
@@ -10,7 +10,7 @@ export const getFeatureImages = createAsyncThunk(
   "/order/getFeatureImages",
   async () => {
     const response = await axios.get(
-      `https://mera-bestie-six.vercel.app/common/feature/get`
+      `https://mera-bestie-six.vercel.app/api/common/feature/get`
     );
 
     return response.data;
@@ -21,7 +21,7 @@ export const addFeatureImage = createAsyncThunk(
   "/order/addFeatureImage",
   async (image) => {
     const response = await axios.post(
-      `https://mera-bestie-six.vercel.app/common/feature/add`,
+      `https://mera-bestie-six.vercel.app/api/common/feature/add`,
       { image }
     );
 
